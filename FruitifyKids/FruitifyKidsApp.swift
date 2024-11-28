75d1834bacf60f24adce28ee2ae1b452067bf5b4
gimhantharuke456 <gimhantharuke456@gmail.com>
2024-11-28 12:23:20 +0530

    Initial Commit

---
+++ FruitifyKids/FruitifyKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitifyKidsApp.swift
+//  FruitifyKids
+//
+//  Created by Gimhan Rajapaksha on 2024-11-28.
+//
+
+import SwiftUI
+
+@main
+struct FruitifyKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
